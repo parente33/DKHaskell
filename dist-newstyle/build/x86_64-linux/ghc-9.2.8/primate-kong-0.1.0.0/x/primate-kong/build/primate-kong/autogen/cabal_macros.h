12813af74fa171e8b77c6e48ee8e6bdbe2e33f9c
parente33 <a107363@alunos.uminho.pt>
2024-04-23 15:03:03 +0100

    assets na pasta já funcionam

--- dist-newstyle/build/x86_64-linux/ghc-9.2.8/primate-kong-0.1.0.0/x/primate-kong/build/primate-kong/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-linux/ghc-9.2.8/primate-kong-0.1.0.0/x/primate-kong/build/primate-kong/autogen/cabal_macros.h
@@ -20,6 +20,16 @@
   (major1) == 4 && (major2) <  16 || \
   (major1) == 4 && (major2) == 16 && (minor) <= 4)
 #endif /* MIN_VERSION_base */
+/* package filepath-1.4.2.2 */
+#ifndef VERSION_filepath
+#define VERSION_filepath "1.4.2.2"
+#endif /* VERSION_filepath */
+#ifndef MIN_VERSION_filepath
+#define MIN_VERSION_filepath(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  4 || \
+  (major1) == 1 && (major2) == 4 && (minor) <= 2)
+#endif /* MIN_VERSION_filepath */
 /* package gloss-1.13.2.2 */
 #ifndef VERSION_gloss
 #define VERSION_gloss "1.13.2.2"
