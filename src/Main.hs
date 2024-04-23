2ee41433f21147f75aa0ce12102e9707dccb9536
parente33 <a107363@alunos.uminho.pt>
2024-04-23 22:32:03 +0100

    TEMOS MÚSICA O JOGO TODO

--- src/Main.hs
+++ src/Main.hs
@@ -12,7 +12,7 @@ import SDL.Mixer
 import System.Environment (getEnv)
 import System.FilePath ((</>))
 import Control.Exception (bracket)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay,forkIO)
 import Control.Monad
 
 main :: IO ()
@@ -20,29 +20,19 @@ main = do
     imgs <- carregarImagens
 
     initialize [InitMP3]
-
     openAudio defaultAudio 256
 
     musicFilePath <- load "Pou music ost - ConnectCliff JumpCliff DashJet Pou.mp3"
 
-    let musicPlaying = True
-
-    SDL.Mixer.play musicFilePath
-
-    let loop = do
-          when musicPlaying $ do
-              threadDelay 1000000
-              loop
-
-    loop
-
-    closeAudio
+    _ <- forkIO $ SDL.Mixer.play musicFilePath
 
     playIO
         janela
         corFundo
         frameRate
-        (Estado {imagens = imgs,modo = MenuInicial Jogar,acao = Parar,jogo = jogo1})
+        (Estado {imagens = imgs, modo = MenuInicial Jogar, acao = Parar, jogo = jogo1})
         (\estado -> desenha estado (jogador (jogo estado)) 0)
         reage
         tempo
+
+    closeAudio
